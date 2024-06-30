128b4713b24750bad0dd14c6b2bfe071200019a8
Mohamed Al Wahaibi <mohamed.alwahaibi2@omanair.com>
2024-06-30 20:07:06 +0400

    fix

--- src/components/GeneralComponents/DataTable/DatatableListNested.vue
+++ src/components/GeneralComponents/DataTable/DatatableListNested.vue
@@ -1,29 +1,29 @@
 <template>
-  <div class="flex flex-wrap gap-2">
-    <span v-if="isArray" v-for="(item, index) in nestedValue" :key="index" class="px-2 py-1 text-xs font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300">{{ item }}</span>
-    <span v-else-if="isObject" class="px-2 py-1 text-xs font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300">{{ nestedValue }}</span>
+<div class="flex flex-wrap gap-2">
+    <span v-if="isArray" v-for="(item, index) in getNestedValue(row, entry.keys)" :key="index" class="px-2 py-1 text-xs font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300">{{ item[entry.field] }}</span>
+    <span v-else-if="isObject" class="px-2 py-1 text-xs font-medium text-blue-800 bg-blue-100 rounded dark:bg-blue-900 dark:text-blue-300">{{ getNestedValue(row, entry.keys)[entry.field] }}</span>
   </div>
 </template>
 
 <script>
-  import { markRaw } from 'vue';
-  
 export default {
   props: ['field', 'row', 'tdProps'],
   computed: {
     entry() {
       const keys = this.field.split('.');
-      return markRaw(keys);
-     
-    },
-    nestedValue() {
-      return this.entry.reduce((obj, key) => (obj && obj[key] !== 'undefined') ? obj[key] : null, this.row);
+      const field = keys.pop();
+      return { keys, field };
     },
     isArray() {
-      return Array.isArray(this.nestedValue);
+      return Array.isArray(this.getNestedValue(this.row, this.entry.keys));
     },
     isObject() {
-      return typeof this.nestedValue === 'object' && !Array.isArray(this.nestedValue);
+      return typeof this.getNestedValue(this.row, this.entry.keys) === 'object';
+    }
+  },
+  methods: {
+    getNestedValue(obj, keys) {
+      return keys.reduce((o, k) => (o || {})[k], obj);
     }
   }
 }
