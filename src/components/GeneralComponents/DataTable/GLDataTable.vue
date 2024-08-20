a25b96d519bcc7ba64979d59d319620009978969
Mohamed Al Wahaibi <mohamed.alwahaibi2@omanair.com>
2024-08-20 11:52:29 +0400

    fix

--- src/components/GeneralComponents/DataTable/GLDataTable.vue
+++ src/components/GeneralComponents/DataTable/GLDataTable.vue
@@ -54,17 +54,17 @@
 
     <div class="overflow-auto rounded-lg dark:text-gray-400 dark:bg-gray-800">
       <table
-        class="w-full h-full max-w-full overflow-hidden bg-white border-separate xl:overflow-auto lg:border-collapse border-spacing-y-3 lg:border-spacing-y-0 dark:border-strokedark dark:bg-boxdark"
+        class="w-full h-full max-w-full overflow-hidden bg-white border-separate xl:overflow-auto lg:!border-collapse border-spacing-y-3 lg:!border-spacing-y-0 dark:border-strokedark dark:bg-boxdark"
       >
         <thead
-          class="hidden text-sm font-normal text-center text-gray-700 lg:table-header-group dark:border-strokedark bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
+          class="hidden text-sm font-normal text-center text-gray-700 lg:!table-header-group dark:border-strokedark bg-gray-50 dark:bg-gray-700 dark:text-gray-400"
         >
           <tr>
             <th
               v-for="(column, index) in columns"
               :key="index"
               v-on:click.prevent="sort(column)"
-              class="w-full px-4 py-2 lg:w-2/12 capitalize"
+              class="w-full px-4 py-2 lg:!w-2/12 capitalize"
             >
               {{ column }}
 
@@ -87,7 +87,7 @@
               v-for="(column, colIndex) in columns"
               :key="colIndex"
               :data-label="column"
-              class="text-pretty before:content-[attr(data-label)] before:font-bold lg:before:content-none flex md:flex-row flex-col justify-between gap-2 lg:table-cell py-4 px-5 lg:py-2.5 lg:px-4 border dark:border-gray-700"
+              class="text-pretty before:content-[attr(data-label)] before:font-bold lg:!before:content-none flex md:flex-row flex-col justify-between gap-2 lg:!table-cell py-4 px-5 lg:!py-2.5 lg:!px-4 border dark:border-gray-700"
             >
               <p class="text-pretty" v-html="item[colIndex]"></p>
             </td>
