809b40eb1011cb35f935765f33a9ffafee1ff986
Mohamed Al Wahaibi <mod.alwahaibi@gmail.com>
2026-05-05 13:30:15 +0400

    fix

--- src/components/GeneralComponents/DataTable/GLDataTableServerSide.vue
+++ src/components/GeneralComponents/DataTable/GLDataTableServerSide.vue
@@ -99,7 +99,7 @@
       </gl-button>
 
 
-      <gl-button @click="exportToExcel" tag="button" button_type="light" :has_border_reduced="false"
+      <gl-button @click="exportToExcel" tag="button" button_type="light" :has_border_reduced="false" classes="rounded-lg lg:rounded-none"
         
         svg_icon='<svg  aria-hidden="true" xmlns="http://www.w3.org/2000/svg"  fill="none" viewBox="0 0 24 24">
                  <path stroke="currentColor" stroke-linejoin="round" stroke-width="2" d="M10 3v4a1 1 0 0 1-1 1H5m14-4v16a1 1 0 0 1-1 1H6a1 1 0 0 1-1-1V7.914a1 1 0 0 1 .293-.707l3.914-3.914A1 1 0 0 1 9.914 3H18a1 1 0 0 1 1 1Z"/>
